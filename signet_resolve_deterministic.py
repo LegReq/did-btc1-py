63360a4fbbf8c4d47d07cfd12159dadf65b88a00
Wip <wip.abramson@gmail.com>
2025-05-27 14:37:12 +0100

    add signet test vector

---
+++ signet_resolve_deterministic.py
@@ -0,0 +1,31 @@
+from libbtc1.resolver import Btc1Resolver
+import asyncio
+import json
+
+async def resolve_deterministic():
+  
+    test_folder_path = "btc1-update-signet-test/update"
+  
+    with open(f"{test_folder_path}/did.txt", 'r') as f:
+        # Read the contents of the file into a variable
+        did_to_resolve = f.read()
+        # Print the names
+        print(did_to_resolve)
+      
+    with open(f"{test_folder_path}/resolutionOptions.json") as f:
+        resolution_options = json.load(f)
+        print(resolution_options)
+
+
+
+    rpc_endpoint = "http://localhost:18443"
+    resolver = Btc1Resolver(rpc_endpoint=rpc_endpoint, rpcuser="polaruser", rpcpassword="polarpass", logging=True)
+    print(resolver.logging)
+    document = await resolver.resolve(did_to_resolve, resolution_options)
+
+    print("Resolved Document")
+    print(json.dumps(document.serialize(), indent=2))
+
+
+
+asyncio.run(resolve_deterministic())
